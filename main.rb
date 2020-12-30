321db1603e8957107938e3c43124316a3bb98707
Vitaly Bogachev <vit.bogachev@gmail.com>
2020-12-30 23:46:21 +0300

    Save output file

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 path_array = []
 path = ''
+file_name = 'all_objects'
 
 files = Dir.glob("#{path}*.txt")
 
@@ -8,3 +9,15 @@ files.each do |file_path|
 end
 
 p path_array
+
+path_array.each do |path|
+  objects = File.open("#{path}", 'r') { |file| file.readlines }
+  objects_array << objects.map! { |object| object.strip }
+end
+
+objects_array.map do |object|
+  object.map do |obj|
+    File.write("#{file_name}.txt", "#{obj}\n", mode: 'a')
+    p obj
+  end
+end
